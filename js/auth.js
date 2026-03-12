766f2c63af9fcd0cc626cc17d25657d8a366f6df
tejuszz <tejuslaiju@gmail.com>
2026-03-12 19:57:20 +0530

    changes to the login system

--- js/auth.js
+++ js/auth.js
@@ -1,20 +1,63 @@
-const users={student:"123",professor:"123",admin:"123"};
+const users={
+student:[
+{id:"251210129",pass:"123"},
+{id:"251210116",pass:"123"},
+{id:"251210095",pass:"123"}
+],
+professor:{id:"teacher@nitdelhi.ac.in",pass:"123"},
+admin:{id:"admin",pass:"123"}
+};
+
+let currentRole="";
+
+function selectRole(role){
+
+currentRole=role;
+
+document.getElementById("roleSelect").style.display="none";
+document.getElementById("loginForm").style.display="flex";
+
+if(role==="student"){
+document.getElementById("roleTitle").innerText="Student Login";
+document.getElementById("username").placeholder="Roll Number";
+}
+
+if(role==="professor"){
+document.getElementById("roleTitle").innerText="Teacher Login";
+document.getElementById("username").placeholder="Email";
+}
+
+if(role==="admin"){
+document.getElementById("roleTitle").innerText="Admin Login";
+document.getElementById("username").placeholder="Admin ID";
+}
+
+}
+
+function goBack(){
+document.getElementById("loginForm").style.display="none";
+document.getElementById("roleSelect").style.display="flex";
+}
 
 function login(e){
-    e.preventDefault();
-    let role=document.querySelector("select").value;
-    let pass=document.querySelector("input[type=password]").value;
+e.preventDefault();
 
-    if(users[role]===pass){
-        localStorage.setItem("role",role);
-        window.location.href=role+".html";
-    }else{
-        alert("Invalid login");
-    }
-    return false;
+let id=document.getElementById("username").value;
+let pass=document.getElementById("password").value;
+
+if(id===users[currentRole].id && pass===users[currentRole].pass){
+
+localStorage.setItem("role",currentRole);
+window.location.href=currentRole+".html";
+
+}else{
+alert("Invalid login");
+}
+
+return false;
 }
 
 function logout(){
-    localStorage.removeItem("role");
-    window.location.href="index.html";
+localStorage.removeItem("role");
+window.location.href="index.html";
 }
